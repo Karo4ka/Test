f9bca6b7489d889371dc7168f10bba9e48eb51ca
Karo4ka <karina.marochkina@icloud.com>
2018-11-27 16:19:03 +0200

    php file

---
+++ ind.php
@@ -0,0 +1,11 @@
+<?php
+class Students {
+    private $Alex,$Pavel, $Karina;
+    pritected function __construct($name, $lastname, $surname, $age)
+{
+    $this->fullName = $name;
+    $this->fullName = $lastname;
+    $this->fullName = $surname;
+    $this->fullName = $age;
+}
+}
